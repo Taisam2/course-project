8541064e8e8d6f76bb2ade1fccefc5bf479f4f62
Taisam2 <maschmid2@gmail.com>
2022-10-04 09:03:59 +0200

    Project setup with all components nested in each other.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Cake with chocolate', 'https://www.thespruceeats.com/thmb/pn4JQhIbIgi0mb_FFkl0Bd_aU_s=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/homemade-raw-vegan-chocolate-recipe-3377425-hero-01-e1ed575a419e4a9a9bc94f3bb1ed1d97.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
