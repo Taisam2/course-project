1b605089cd9d583c591530437f5d3719662cbbd3
Taisam2 <maschmid2@gmail.com>
2022-10-04 09:50:54 +0200

    filled components with content, basic visual presentation of content. Model classes created.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from 'src/app/models/recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'Cake with chocolate', 'https://www.thespruceeats.com/thmb/pn4JQhIbIgi0mb_FFkl0Bd_aU_s=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/homemade-raw-vegan-chocolate-recipe-3377425-hero-01-e1ed575a419e4a9a9bc94f3bb1ed1d97.jpg')
+    new Recipe('Test Recipe', 'Cake with chocolate', 'https://www.thespruceeats.com/thmb/pn4JQhIbIgi0mb_FFkl0Bd_aU_s=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/homemade-raw-vegan-chocolate-recipe-3377425-hero-01-e1ed575a419e4a9a9bc94f3bb1ed1d97.jpg'),
+    new Recipe('Test Recipe 2', 'Cake with chocolate', 'https://www.thespruceeats.com/thmb/pn4JQhIbIgi0mb_FFkl0Bd_aU_s=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/homemade-raw-vegan-chocolate-recipe-3377425-hero-01-e1ed575a419e4a9a9bc94f3bb1ed1d97.jpg'),
+    new Recipe('Test Recipe 3', 'Cake with chocolate', 'https://www.thespruceeats.com/thmb/pn4JQhIbIgi0mb_FFkl0Bd_aU_s=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/homemade-raw-vegan-chocolate-recipe-3377425-hero-01-e1ed575a419e4a9a9bc94f3bb1ed1d97.jpg'),
   ];
 
   constructor() { }
